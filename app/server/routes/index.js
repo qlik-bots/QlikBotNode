661ecc050ee87fe37982826598bcfbed283b5305
Yianni Ververis <yiannisver@gmail.com>
2018-01-09 08:58:17 -0500

    more fixes

--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -6,7 +6,7 @@
 */
 
 const express = require('express');
-const router = express.router();
+const router = express.Router();
 const path = require('path');
 
 // frontend routes =========================================================
