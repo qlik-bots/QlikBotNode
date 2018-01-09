632a1be3e98fb787bb91e1ec648b183f278f0e2d
Yianni Ververis <yiannisver@gmail.com>
2018-01-09 13:46:19 -0500

    lint helpdesk

--- app/server/routes/api/sense-bot/microsoft/helpdesk.js
+++ app/server/routes/api/sense-bot/microsoft/helpdesk.js
@@ -1,18 +1,78 @@
-/*************
- * HELPDESK
- *************/
-const site = require('../../../../models/sense-bot')
+/**
+ * Heldesk Dialog with all of the available commands
+ * @module routes/api/sense-bot/microsoft::helpdesk()
+ * @author yianni.ververis@qlik.com
+*/
+
+const site = require('../../../../models/sense-bot');
 const config = require('../../../../config.json');
 let engine = null;
 const qvf = config.qvf.helpdesk;
 let text = config.text.en;
 
-module.exports = function (bot, builder) {	
+module.exports = function (bot, builder) {
+	/**
+	 * Helpdesk High Priority Cases KPI
+	 * @function helpdeskHighPriorityCases()
+	 * @param {string} message - The message to send to all users in the database.
+	 * @author yianni.ververis@qlik.com
+	*/
+	async function helpdeskHighPriorityCases (session) {
+		try {
+			let result = await engine.kpi("Count( {$<Priority={'High'}, Status -={'Closed'} >} Distinct %CaseId )");
+			session.send(text.helpdesk.highPriorityCases.text, result[0][0].qText);
+			site.logger.info(`loaded`, { route: `api/sense-bot/microsoft::helpdeskHighPriorityCases()` });			
+		}
+		catch (error) {
+			site.logger.info(`error: ${error}`, { route: `api/sense-bot/microsoft::helpdeskHighPriorityCases()` });
+		}
+	}
+	
+	/**
+	 * Helpdesk Medium Priority Cases KPI
+	 * @function helpdeskMediumPriorityCases()
+	 * @param {string} message - The message to send to all users in the database.
+	 * @author yianni.ververis@qlik.com
+	*/
+	async function helpdeskMediumPriorityCases (session) {
+		try {
+			let result = await engine.kpi("Count( {$<Priority={'Medium'}, Status -={'Closed'} >} Distinct %CaseId )");
+			session.send(text.helpdesk.mediumPriorityCases.text, result[0][0].qText);
+			site.logger.info(`loaded`, { route: `api/sense-bot/microsoft::helpdeskMediumPriorityCases()` });			
+		}
+		catch (error) {
+			site.logger.info(`error: ${error}`, { route: `api/sense-bot/microsoft::helpdeskMediumPriorityCases()` });
+		}
+	}
+	
+	/**
+	 * Helpdesk Low Priority Cases KPI
+	 * @function helpdeskLowPriorityCases()
+	 * @param {string} message - The message to send to all users in the database.
+	 * @author yianni.ververis@qlik.com
+	*/
+	async function helpdeskLowPriorityCases (session) {
+		try {
+			let result = await engine.kpi("Count( {$<Priority={'Low'}, Status -={'Closed'} >} Distinct %CaseId )");
+			session.send(text.helpdesk.lowPriorityCases.text, result[0][0].qText);
+			site.logger.info(`loaded`, { route: `api/sense-bot/microsoft::helpdeskLowPriorityCases()` });			
+		}
+		catch (error) {
+			site.logger.info(`error: ${error}`, { route: `api/sense-bot/microsoft::helpdeskLowPriorityCases()` });
+		}
+	}
+		
+	/**
+	 * Helpdesk Low Priority Cases KPI
+	 * @function helpdeskLowPriorityCases()
+	 * @param {string} message - The message to send to all users in the database.
+	 * @author yianni.ververis@qlik.com
+	*/
 	bot.dialog('helpdesk', async function (session) {
 		try {
 			let sessionLanguage = session.preferredLocale().split('-')[0];
 			text = (config.text[sessionLanguage]) ? config.text[sessionLanguage] : config.text.en;
-			engine = await new site.enigma(qvf)
+			engine = await new site.Enigma(qvf);
 			let msg = new builder.Message(session)
 				.attachmentLayout(builder.AttachmentLayout.list)
 				.attachments([
@@ -30,20 +90,17 @@ module.exports = function (bot, builder) {
 							builder.CardAction.postBack(session, "high-priority-cases", text.helpdesk.highPriorityCases.button),
 							builder.CardAction.postBack(session, "medium-priority-cases", text.helpdesk.mediumPriorityCases.button),
 							builder.CardAction.postBack(session, "low-priority-cases", text.helpdesk.lowPriorityCases.button),
-							builder.CardAction.postBack(session, "exit", text.exit.button),
+							builder.CardAction.postBack(session, "exit", text.exit.button)
 						])
 				]);
 			site.logger.info(`loaded`, { route: `api/sense-bot/microsoft::helpdesk-main()` });
 			switch (session.message.text.toLocaleLowerCase()) {
 				case 'high-priority-cases':
 					return helpdeskHighPriorityCases(session, msg2);
-					break;
 				case 'medium-priority-cases':
 					return helpdeskMediumPriorityCases(session);
-					break;
 				case 'low-priority-cases':
 					return helpdeskLowPriorityCases(session);
-					break;
 				case 'exit':
 					engine.disconnect();
 					session.endDialog(text.exit.text);
@@ -59,38 +116,4 @@ module.exports = function (bot, builder) {
 		}
 	})
 		.triggerAction({ matches: /^Helpdesk$/i });
-	
-	async function helpdeskHighPriorityCases (session, msg2) {
-		try {
-			let result = await engine.kpi("Count( {$<Priority={'High'}, Status -={'Closed'} >} Distinct %CaseId )");
-			session.send(text.helpdesk.highPriorityCases.text, result[0][0].qText);
-			// session.send(msg2)
-			site.logger.info(`loaded`, { route: `api/sense-bot/microsoft::helpdeskHighPriorityCases()` });			
-		}
-		catch (error) {
-			site.logger.info(`error: ${error}`, { route: `api/sense-bot/microsoft::helpdeskHighPriorityCases()` });
-		}
-	}
-	
-	async function helpdeskMediumPriorityCases (session) {
-		try {
-			let result = await engine.kpi("Count( {$<Priority={'Medium'}, Status -={'Closed'} >} Distinct %CaseId )");
-			session.send(text.helpdesk.mediumPriorityCases.text, result[0][0].qText);
-			site.logger.info(`loaded`, { route: `api/sense-bot/microsoft::helpdeskMediumPriorityCases()` });			
-		}
-		catch (error) {
-			site.logger.info(`error: ${error}`, { route: `api/sense-bot/microsoft::helpdeskMediumPriorityCases()` });
-		}
-	}
-	
-	async function helpdeskLowPriorityCases (session) {
-		try {
-			let result = await engine.kpi("Count( {$<Priority={'Low'}, Status -={'Closed'} >} Distinct %CaseId )")
-			session.send(text.helpdesk.lowPriorityCases.text, result[0][0].qText);
-			site.logger.info(`loaded`, { route: `api/sense-bot/microsoft::helpdeskLowPriorityCases()` });			
-		}
-		catch (error) {
-			site.logger.info(`error: ${error}`, { route: `api/sense-bot/microsoft::helpdeskLowPriorityCases()` });
-		}
-	}
-}
+};
