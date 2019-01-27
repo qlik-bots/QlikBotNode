69dc56f366dbdd8f4ae5c2515f26e0090bc70214
dacopan <dacopanbsc@gmail.com>
2019-01-27 18:51:16 -0500

    add getMeasure method, as QlikBotNet

--- app/server/models/utilities/Enigma.js
+++ app/server/models/utilities/Enigma.js
@@ -4,7 +4,7 @@
  * @param {object} config - The configuration for host, database name, username and password
  * @description
  * Connect to the Engine API with Enigma.js
-*/
+ */
 
 const logger = require('./Logger');
 const enigma = require('enigma.js');
@@ -27,6 +27,7 @@ const Enigma = class {
         this.global = null;
         this.app = null;
     }
+
     async connect() {
         try {
             // Default for anonymous connection
@@ -60,21 +61,22 @@ const Enigma = class {
             if (this._input.appId) {
                 this.app = await this.global.openDoc(this._input.appId);
             }
-            logger.info("Loaded!", { model: `models/utilities/Enigma::connect()` });
+            logger.info("Loaded!", {model: `models/utilities/Enigma::connect()`});
             return true;
-        }
-        catch (error) {
-            logger.info(`error: ${error}`, { model: `models/utilities/Enigma::connect()` });
+        } catch (error) {
+            logger.info(`error: ${error}`, {model: `models/utilities/Enigma::connect()`});
             return error;
         }
     }
+
     disconnect() {
         if (this.session) {
             this.session.close();
             this.clients = [];
-            logger.log(`Connection closed: `, { model: `Enigma` });
+            logger.log(`Connection closed: `, {model: `Enigma`});
         }
     }
+
     async getDocList() {
         try {
             let list = await this.global.getDocList();
@@ -86,14 +88,14 @@ const Enigma = class {
                     'thumb': n.qThumbnail.qUrl
                 });
             }
-            logger.log(`Apps on this Engine that the configured user can open: ${apps}`, { model: `Enigma` });
+            logger.log(`Apps on this Engine that the configured user can open: ${apps}`, {model: `Enigma`});
             return apps;
-        }
-        catch (error) {
-            logger.info(`error: ${error}`, { model: `models/utilities/Enigma::getDocList()` });
+        } catch (error) {
+            logger.info(`error: ${error}`, {model: `models/utilities/Enigma::getDocList()`});
             return error;
         }
     }
+
     async kpiMulti(exprs) {
         try {
             await this.connect();
@@ -103,21 +105,21 @@ const Enigma = class {
                 results.push(result[0]);
             }
             return results;
-        }
-        catch (error) {
-            logger.error(`error: ${JSON.stringify(error)}`, { model: `Enigma::kpiMulti()` });
+        } catch (error) {
+            logger.error(`error: ${JSON.stringify(error)}`, {model: `Enigma::kpiMulti()`});
         }
     }
+
     async kpi(expr) {
         try {
             await this.connect();
             let result = await this.getHyperCube([], [expr], 1);
             return result;
-        }
-        catch (error) {
-            logger.error(`error: ${JSON.stringify(error)}`, { model: `Enigma::kpi()` });
+        } catch (error) {
+            logger.error(`error: ${JSON.stringify(error)}`, {model: `Enigma::kpi()`});
         }
     }
+
     async getFieldList(field, limit, returnModel) {
         try {
             await this.connect();
@@ -143,7 +145,7 @@ const Enigma = class {
                                 "qv": ""
                             },
                             "qSortByGreyness": 0
-                        }],                        
+                        }],
                         "qNumberPresentations": [],
                         "qReverseSort": false,
                         "qActiveField": 0,
@@ -167,11 +169,11 @@ const Enigma = class {
                 let layout = await model.getLayout();
                 return layout.qListObject.qDataPages[0].qMatrix;
             }
-        }
-        catch (error) {
-            logger.error(`error: ${JSON.stringify(error)}`, { model: `Enigma::getFieldList()` });
+        } catch (error) {
+            logger.error(`error: ${JSON.stringify(error)}`, {model: `Enigma::getFieldList()`});
         }
     }
+
     async getList(qType) {
         try {
             await this.connect();
@@ -193,13 +195,13 @@ const Enigma = class {
                     list = await this.app.createSessionObject(obj);
                     layout = await list.getLayout();
                     return layout.qFieldList.qItems;
-                
+
                 case "DimensionList":
                     obj = {
                         "qInfo": {
                             "qId": "",
                             "qType": "DimensionList"
-                        },                        
+                        },
                         "qDimensionListDef": {}
                     };
                     list = await this.app.createSessionObject(obj);
@@ -217,33 +219,36 @@ const Enigma = class {
                     list = await this.app.createSessionObject(obj);
                     layout = await list.getLayout();
                     return layout.qSelectionObject.qSelections;
-                
+
                 default:
                     return `"${qType} is not available!"`
             }
-        }
-        catch (error) {
-            logger.error(`error: ${JSON.stringify(error)}`, { model: `Enigma::getList()` });
+        } catch (error) {
+            logger.error(`error: ${JSON.stringify(error)}`, {model: `Enigma::getList()`});
         }
     }
+
     async selections() {
         try {
             await this.connect();
-        }
-        catch (error) {
-            logger.error(`error: ${JSON.stringify(error)}`, { model: `Enigma::kpi()` });
+        } catch (error) {
+            logger.error(`error: ${JSON.stringify(error)}`, {model: `Enigma::kpi()`});
         }
     }
+
     async select(field, value) {
         try {
             await this.connect();
-            let result = await this.app.selectAssociations({"qSearchFields": [field],"qContext": "CurrentSelections"},[value],0);
+            let result = await this.app.selectAssociations({
+                "qSearchFields": [field],
+                "qContext": "CurrentSelections"
+            }, [value], 0);
             return result;
-        }
-        catch (error) {
-            logger.error(`error: ${JSON.stringify(error)}`, { model: `Enigma::select()` });
+        } catch (error) {
+            logger.error(`error: ${JSON.stringify(error)}`, {model: `Enigma::select()`});
         }
     }
+
     async clear(field) {
         try {
             await this.connect();
@@ -253,11 +258,11 @@ const Enigma = class {
                 await this.app.clearAll();
                 return 'All Selections are cleared ';
             }
-        }
-        catch (error) {
-            logger.error(`error: ${JSON.stringify(error)}`, { model: `Enigma::clearAll()` });
+        } catch (error) {
+            logger.error(`error: ${JSON.stringify(error)}`, {model: `Enigma::clearAll()`});
         }
     }
+
     async getHyperCube(dimensions, measures, limit) {
         try {
             let qDimensions = [],
@@ -307,9 +312,19 @@ const Enigma = class {
             let list = await this.app.createSessionObject(obj);
             let layout = await list.getLayout();
             return layout.qHyperCube.qDataPages[0].qMatrix;
+        } catch (error) {
+            logger.error(`error: ${JSON.stringify(error)}`, {model: `Enigma::getHyperCube()`});
         }
-        catch (error) {
-            logger.error(`error: ${JSON.stringify(error)}`, { model: `Enigma::getHyperCube()` });
+    }
+
+    async getMeasure(id) {
+        try {
+            await this.connect();
+            let measure = await this.app.getMeasure(id);
+            let layout = await measure.getLayout();
+            return layout.qMeasure;
+        } catch (error) {
+            logger.error(`error: ${JSON.stringify(error)}`, {model: `Enigma::getMeasure()`});
         }
     }
 };
